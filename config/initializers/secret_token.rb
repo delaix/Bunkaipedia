1522296b0d5bb24eb68a8908f8123fa85b50ebec
Andrew de Laix <delaix@mac.com>
2012-01-20 19:00:06 -0600

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bunkaipedia::Application.config.secret_token = 'ed50c57170687f13e3730c1faed8f685c1577b71d5ca394e97755ff8913f41d99be95a163b058fb98e6d1e10d4d3179f6eb088c85c6df270a5a6931e4a901d0d'
